4997ba8a39d08bbf414fd8bbef08a99985e4ce7c
Hatoris <florianxbernard@gmail.com>
2024-09-30 08:38:44 +0200

    add missing constant

--- custom_components/candy/const.py
+++ custom_components/candy/const.py
@@ -19,6 +19,7 @@ UNIQUE_ID_OVEN = "{0}-oven"
 UNIQUE_ID_OVEN_TEMP = "{0}-oven-temp"
 UNIQUE_ID_DISHWASHER = "{0}-dishwasher"
 UNIQUE_ID_DISHWASHER_REMAINING_TIME = "{0}-dishwasher_remaining_time"
+UNIQUE_ID_DISHWASHER_PROGRAM = "{0}-dishwasher_current_program"
 
 DEVICE_NAME_WASHING_MACHINE = "Washing machine"
 DEVICE_NAME_TUMBLE_DRYER = "Tumble dryer"
